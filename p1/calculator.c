4c65abe62f8ee6e04ef2c97b1b80b04703684cbf
Zoey <114689677+klub-ice@users.noreply.github.com>
2026-09-02 14:58:21 -0500

    Update calculator.c

--- p1/calculator.c
+++ p1/calculator.c
@@ -6,16 +6,54 @@
 int calculate() {
     int num1, num2;
     char op;
+
     printf("Enter operation\n");
-    scanf("%c", &op);
+    if (scanf(" %c", &op) != 1) {
+        printf("Error: Invalid input\n");
+        return 1;
+    }
+
     printf("Enter first number\n");
-    scanf("%d", &num1);
+    if (scanf("%d", &num1) != 1) {
+        printf("Error: Invalid input\n");
+        return 1;
+    }
+
+    if (op == '2') {
+        printf("Result: %d\n", num1 * num1);
+        return 0;
+    }
+
+    if (op == '3') {
+        printf("Result: %d\n", num1 * num1 * num1);
+        return 0;
+    }
+
     printf("Enter second number\n");
-    scanf("%d", &num2);
+    if (scanf("%d", &num2) != 1) {
+        printf("Error: Invalid input\n");
+        return 1;
+    }
 
     if (op == '+') {
         printf("Result: %d\n", num1 + num2);
-
+    }
+    else if (op == '-') {
+        printf("Result: %d\n", num1 - num2);
+    }
+    else if (op == '*') {
+        printf("Result: %d\n", num1 * num2);
+    }
+    else if (op == '/') {
+        if (num2 == 0) {
+            printf("Error: Division by zero\n");
+            return 1;
+        }
+        printf("Result: %d\n", num1 / num2);
+    }
+    else {
+        printf("Error: Invalid operator\n");
+        return 1;
     }
 
     return 0;
