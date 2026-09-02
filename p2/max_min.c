835490a4cc868a9b38428271b62d1fe3407dd8c2
Zoey <114689677+klub-ice@users.noreply.github.com>
2026-09-02 15:47:29 -0500

    Update max_min.c

--- p2/max_min.c
+++ p2/max_min.c
@@ -1,3 +1,13 @@
 //
 // Created by zoey on 9/2/2026.
 //
+#include <stdio.h>
+
+int max_min(int array) {
+    return 0;
+}
+
+int main() {
+    int array [10] = {500, 1, 255, 7, -12, 40, 42, 999, 50, 227};
+    return 0;
+}
