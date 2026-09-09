2cced0ed6d07fd049a8a19619d9eb0b4a5f7b374
klub-ice <114689677+klub-ice@users.noreply.github.com>
2026-09-09 12:52:31 -0500

    Update p2.c

--- p2.c
+++ p2.c
@@ -7,19 +7,19 @@ int main() {
     int array [10] = {500, 1, 255, 7, -12, 40, 42, 999, 50, 227};
     int choice;
 
-    printf("Choose m n or b:" );
+    printf("Choose m n or b:" ); // choices for user to pick
     scanf("%c", choice);
 
-    int max = array[0];
-    int min = array[0];
+    int max = array[0]; // sets max to first array slot
+    int min = array[0];// sets min to first array slot
 
-    for(int i = i; i < 10; i++) {
+    for(int i = i; i < 10; i++) { // loops through array to find max and min
         if (array[i] > max)
             max = array[i];
         if (array[i] < min)
             min = array[i];
     }
-
+    // if statement tree to decide what happens based on user input
     if (choice == 'm') {
         printf(" %d\n", max);
     }
