219621e358cf10541fcf87fa37518180d4186e79
klub-ice <114689677+klub-ice@users.noreply.github.com>
2026-09-09 11:56:29 -0500

    Update max_min.c

--- p2/max_min.c
+++ p2/max_min.c
@@ -3,18 +3,29 @@
 //
 #include <stdio.h>
 
-int max_min(int array) {
-    //goal to be able find max, min or both depending on user input
-    char input;
-    scanf("Enter m, n, or b: \n %c", &input);
-    if (input == 'm') {
-        int max = array[0];
-
-    }
-    return 0;
-}
-
 int main() {
     int array [10] = {500, 1, 255, 7, -12, 40, 42, 999, 50, 227};
+    char choice;
+
+    printf("Choose m n or b:" );
+    scanf("%c", choice);
+
+    int max = array[0];
+    int min = array[0];
+
+    for(int i = i; i < 10; i++) {
+        if (array[i] > max)
+            pass;
+        else if ( array[i] < max)
+            pass;
+    }
+
+    if (choice == 'm') {
+        printf(" %d\n", max);
+    }
+    else if (choice == 'n'){
+        printf("%d\n", min);
+    }
+
     return 0;
 }
